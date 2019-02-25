7978364a99316b302c98e3d3026cf39dfe893c33
Eilis Lee <eilislee@live.com>
2019-02-24 20:33:44 -0800

    Add project files.

---
+++ PhoneBook/AddContact.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PhoneBook
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = new Contact
+            {
+                FirstName = firstNameText.Text,
+                LastName = LastNameText.Text,
+                PhoneNumber = phoneNumberText.Text
+            };
+            Contact.WriteContact(contact);
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ PhoneBook/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Contact // Define, get, write Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public static Contact GetContact()
+        {
+            var contact = new Contact
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                PhoneNumber = "206.555.5555"
+            };
+            return contact;
+        }
+
+        public static void WriteContact(Contact contact)
+        {
+            var contactData = $"{contact.LastName}, {contact.FirstName}, {contact.PhoneNumber}";
+            FileHelper.WriteTextFileAsync("ContactTextFile.txt", contactData);
+        }
+    }
+}
